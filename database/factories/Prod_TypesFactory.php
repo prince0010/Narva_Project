f0b6de5e029c4650746a9ef1628c3065433ce66a
PRINCE <princenagac12@gmail.com>
2024-02-11 08:02:46 +0800

    User, Supplier, Prod_Type Seed

---
+++ database/factories/Prod_TypesFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Prod_Types>
+ */
+class Prod_TypesFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+
+            'product_name' => $this->faker->name(), 
+            
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,15 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Prod_Types;
+use App\Models\Supplier;
+use App\Models\User;
+use Closure;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\Console\Helper\ProgressBar;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +26,60 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // $this->call([
+        //     UserSeeder::class,
+        // ]);
+
+        // User
+        $this->command->warn(PHP_EOL . 'Creating User....');
+        $user = $this->withProgressBar(1, fn () => User::factory(1)->create([
+            'name' => 'Kenth Balagonsa',
+            'email' => 'kbalagonsa001@gmail.com',
+            'password' => Hash::make('kenthbalagonsa221'),
+        ]));
+        $this->command->info('User Data is Created.');
+
+        // Product Types
+        $this->command->warn(PHP_EOL . 'Creating Product Type....');
+        $producttype = $this->withProgressBar(10, fn () => Prod_Types::factory()->count(10)->create());
+        $this->command->info('Product Type is Created.');
+
+        // Supplier
+        $this->command->warn(PHP_EOL . 'Creating Supplier....');
+        $producttype = $this->withProgressBar(200, fn () => Supplier::factory()->count(20)->create());
+        $this->command->info('Supplier is Created.');
+
+        // Products
+        
+       
+    }
+
+
+
+
+
+
+    // With Progress Bar Function
+    protected function withProgressBar(int $amount, Closure $createCollectionOfOne): Collection
+    {
+        $progressBar = new ProgressBar($this->command->getOutput(), $amount);
+
+        $progressBar->start();
+
+        $items = new Collection();
+
+        foreach (range(1, $amount) as $i) {
+            $items = $items->merge(
+                $createCollectionOfOne()
+            );
+            $progressBar->advance();
+        }
+
+        $progressBar->finish();
+
+        $this->command->getOutput()->writeln('');
+
+        return $items;
     }
 }
