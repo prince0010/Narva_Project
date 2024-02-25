80e813a8c9bcbc6d8fe7ceeac8f986ae76e1eb01
PRINCE <princenagac12@gmail.com>
2024-02-25 21:07:00 +0800

    Eloquent Datas and Update Some Factory Realistic Datas

--- database/factories/ProductsFactory.php
+++ database/factories/ProductsFactory.php
@@ -15,14 +15,14 @@ class ProductsFactory extends Factory
    
     public function definition()
     {
-
+        // $randomizedPriceCode = $this->faker->randomElement(['O', 'R', 'G', 'R', 'A', 'N', 'I', 'Z', 'E', 'D', 'B'], 3);
             return [
                 'prod_type_ID' => Prod_Types::factory(),
                 'part_num' =>  $this->faker->randomNumber(2),
                 'part_name' => $this->faker->randomElement(['Head Gasket Carbon', 'Wheel Cylinder Assy', 'Head Gasket', 'Water Pump', 'Battery Relay', 'Rotor Disc', 'Valve Cover Gasket', 'Brake Shoes']),
                 'brand'=>  $this->faker->randomElement(['ANLD', 'GMB', 'F-MANGO1', 'BENDIX', 'VIC', 'POWER PLUS', 'NEW ERA', 'MUGOL']),
                 'model'=>  $this->faker->randomElement(['NISSAN NAVARRA 2022', 'SUZUKI', 'J2 2.7 KIA', 'JT 3.0', 'KIA KC700', 'NISSAN CALIBRE', 'F5A', 'JT/ PREGIO 3.0/BONGO']),
-                'price_code' =>  $this->faker->numerify('ABC###'),
+                'price_code' => implode('', $this->faker->randomElements(['O', 'R', 'G', 'A', 'N', 'I', 'Z', 'E', 'D', 'B'], 3)), // Extract a Random 3 characters based on the randomizedPriceCode and Convert Array to String
             ];
 
 }
